e14d91cd58f9c6a855e7cf0304000fe89f897fd9
l00139962 <91684011+l00139962@users.noreply.github.com>
2021-12-11 18:56:16 +0000

    testequals and testusername added

--- MavenBuild/src/test/java/ie/lyit/adamsfam/UserTest.java
+++ MavenBuild/src/test/java/ie/lyit/adamsfam/UserTest.java
@@ -6,14 +6,18 @@ public class UserTest extends TestCase {
 	
 
 	
+
+	//Create a user 
+	User mainUser = new User("Brian", "B", "l00139962", "verySecure", "123", "l00139962@student.lyit.ie", 345);
+	
 	//tests if the user fname and sname are set
-	public void testUsersNames() {
+	public void testTheUsersNames() {
 		//create the user object
-		User usr = new User("Brian", "B");
+		//User mainUser = new User("Brian", "B");
 		
 		//methods to test 
-		String fnameVal = usr.getFname();	
-		String snameVal = usr.getSname();
+		String fnameVal = mainUser.getFname();	
+		String snameVal = mainUser.getSname();
 		
 		//check if the value matches
 		assertEquals(fnameVal, "Brian");
@@ -21,6 +25,24 @@ public class UserTest extends TestCase {
 
 		
 		
+	}
+
+	//test for username
+	public void testUserName(){
+
+		//check if the username is set
+		assertEquals(mainUser.username, "l00139962");
+	}
+
+	//test for equality
+	public void testEquals(){
+		//second user
+		User secondUser = new User("Brian2", "B2", "l00139962", "verySecureToo", "123", "l00139962@student.lyit.ie", 345);
+
+		//check if the two objects are the same
+		assertEquals(mainUser.equals(secondUser), true);
+
+
 	}
 	
 	
