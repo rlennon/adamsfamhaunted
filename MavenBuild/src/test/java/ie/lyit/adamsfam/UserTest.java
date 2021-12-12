d5db1219793c8d2a7f6a899757266292ad4cfd55
l00139962 <91684011+l00139962@users.noreply.github.com>
2021-12-12 19:27:10 +0000

    UserTest Updated working

--- MavenBuild/src/test/java/ie/lyit/adamsfam/UserTest.java
+++ MavenBuild/src/test/java/ie/lyit/adamsfam/UserTest.java
@@ -1,5 +1,7 @@
 package ie.lyit.adamsfam;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class UserTest extends TestCase {
@@ -11,6 +13,7 @@ public class UserTest extends TestCase {
 	User mainUser = new User("Brian", "B", "l00139962", "verySecure", "123", "l00139962@student.lyit.ie", 345);
 	
 	//tests if the user fname and sname are set
+	@Test
 	public void testTheUsersNames() {
 		//create the user object
 		//User mainUser = new User("Brian", "B");
@@ -28,13 +31,15 @@ public class UserTest extends TestCase {
 	}
 
 	//test for username
+	@Test
 	public void testUserName(){
 
 		//check if the username is set
-		assertEquals(mainUser.username, "l00139962");
+		assertEquals(mainUser.getUsername(), "l00139962");
 	}
 
 	//test for equality
+	@Test
 	public void testEquals(){
 		//second user
 		User secondUser = new User("Brian2", "B2", "l00139962", "verySecureToo", "123", "l00139962@student.lyit.ie", 345);
